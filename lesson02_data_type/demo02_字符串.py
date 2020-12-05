cd2f9d359b328a0c23c28e7301b5171a2bc85c36
cong <237886015@qq.com>
2020-12-05 11:15:00 +0800

    添加初始化文件

---
+++ lesson02_data_type/demo02_字符串.py
@@ -0,0 +1,11 @@
+#字符串表示：两个引号中间的数据
+#单引号和双引号，三个引号
+username = "yuz"
+username = 'yuz'
+username = '''yuz'''
+
+#三个引号：可以表示多行字符串
+last_name = "wang" \
+            "gjjhj" \
+            "jgj"
+print(last_name)
