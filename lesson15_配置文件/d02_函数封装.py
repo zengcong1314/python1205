adce30e4ddae8b638029147941c84b9a36847feb
zc <237886015@qq.com>
2021-01-07 00:09:18 +0800

    【0104 配置文件】

--- lesson15_配置文件/d02_函数封装.py
+++ lesson15_配置文件/d02_函数封装.py
@@ -1,6 +1,10 @@
 #导入信息放在模块的最上面
 import logging
-def get_log(logname="root",logger_level="DEBUG",stream_handler_level="DEBUG",file=None,file_handler_level="DEBUG",fmt_str="time: %(asctime)s---%(levelname)s:%(name)s:%(message)s---%(filename)s---%(lineno)s"):
+def get_log(logname="root",
+            logger_level="DEBUG",
+            stream_handler_level="DEBUG",
+            file=None,file_handler_level="DEBUG",
+            fmt_str="time: %(asctime)s---%(levelname)s:%(name)s:%(message)s---%(filename)s---%(lineno)s"):
     """logging封装"""
 
     # 获取日志收集器 logger
