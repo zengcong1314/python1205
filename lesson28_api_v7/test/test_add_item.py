5307418ddd8d25889fc61b9e67abf8bab97ce9f8
zc <237886015@qq.com>
2021-02-07 17:49:44 +0800

    0203审核接口

--- lesson28_api_v7/test/test_add_item.py
+++ lesson28_api_v7/test/test_add_item.py
@@ -5,13 +5,13 @@ from middleware.handler import Handler
 
 data = Handler.excel.read_dict("add")
 @pytest.mark.parametrize("info",data)
-def test_add(info,login,db):
+def test_add_loan(info,loan_login,db):
     if "#member_id#" in info["json"]:
-        info["json"] = info["json"].replace("#member_id#",str(login["id"]))
+        info["json"] = info["json"].replace("#member_id#",str(loan_login["id"]))
     if "#wrong_member_id#" in info["json"]:
-        info["json"] = info["json"].replace("#wrong_member_id#",str(login["id"]+10))
+        info["json"] = info["json"].replace("#wrong_member_id#",str(loan_login["id"]+10))
     headers = json.loads(info["headers"])
-    headers['Authorization'] = login['token']
+    headers['Authorization'] = loan_login['token']
     res = requests.request(method=info['method'],
                      url=Handler.yaml_config['host'] + info["url"],
                      headers=headers,
---
+++ lesson28_api_v7/test/test_audit.py
@@ -0,0 +1,5 @@
+"""审核项目"""
+
+def test_audit(admin_login,add_loan):
+    """审核接口"""
+    print(admin_login)
