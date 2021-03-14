a9b4eed574279207e6f1ba92d969e3876278c975
cong <237886015@qq.com>
2021-03-15 00:01:13 +0800

    【0311JS】

--- web/lesson6/d2.py
+++ web/lesson6/d2.py
@@ -12,3 +12,9 @@ js_code = "let input = document.getElementById('train_date');input.readOnly=fals
 driver.execute_script(js_code)
 time.sleep(5)
 
+# 可以把 python和JS 混用
+input_element = driver.find_element('id','train_date')
+js_code = "input.readOnly=false;input.value='2021-03-11'"
+
+
+
